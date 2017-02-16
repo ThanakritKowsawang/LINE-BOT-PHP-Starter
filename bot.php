155247b53f442221800ddd2d482d360d06901a61
ThanakritKowsawang <krit111ays@gmail.com>
2017-02-16 18:13:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'ดี',
-				'text' => $text
+				'ดีจ้า' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
