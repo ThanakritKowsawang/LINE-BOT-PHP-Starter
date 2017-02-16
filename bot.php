317754fdbc320c21cd66353c753bd59982e8d14a
ThanakritKowsawang <krit111ays@gmail.com>
2017-02-16 18:16:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,16 +10,16 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'ดี') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['ดี'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'ดี',
-				'ดี' => $text
+				'type' => 'text',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
